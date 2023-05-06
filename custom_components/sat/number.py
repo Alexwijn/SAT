1516108ef521410d01c3563caf753ae57d685b11
Alex Wijnholds <alexw@asclub.eu>
2023-05-06 15:23:02 +0200

    Make sure we prefix it

--- custom_components/sat/number.py
+++ custom_components/sat/number.py
@@ -3,9 +3,9 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import CONF_MODE, MODE_OPENTHERM
-from .opentherm import number
+from .opentherm import number as opentherm_number
 
 
 async def async_setup_entry(_hass: HomeAssistant, _config_entry: ConfigEntry, _async_add_entities: AddEntitiesCallback):
     if _config_entry.data.get(CONF_MODE) == MODE_OPENTHERM:
-        await number.async_setup_entry(_hass, _config_entry, _async_add_entities)
+        await opentherm_number.async_setup_entry(_hass, _config_entry, _async_add_entities)
