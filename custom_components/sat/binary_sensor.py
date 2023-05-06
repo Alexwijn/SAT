42f019dc6605664dc54796f3567bbdc97bbaecd5
Alex Wijnholds <alexw@asclub.eu>
2023-05-06 15:23:02 +0200

    Make sure we prefix it

--- custom_components/sat/binary_sensor.py
+++ custom_components/sat/binary_sensor.py
@@ -3,9 +3,9 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import CONF_MODE, MODE_OPENTHERM
-from .opentherm import binary_sensor
+from .opentherm import binary_sensor as opentherm_binary_sensor
 
 
 async def async_setup_entry(_hass: HomeAssistant, _config_entry: ConfigEntry, _async_add_entities: AddEntitiesCallback):
     if _config_entry.data.get(CONF_MODE) == MODE_OPENTHERM:
-        await binary_sensor.async_setup_entry(_hass, _config_entry, _async_add_entities)
+        await opentherm_binary_sensor.async_setup_entry(_hass, _config_entry, _async_add_entities)
--- custom_components/sat/sensor.py
+++ custom_components/sat/sensor.py
@@ -3,9 +3,9 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import CONF_MODE, MODE_OPENTHERM
-from .opentherm import sensor
+from .opentherm import sensor as opentherm_sensor
 
 
 async def async_setup_entry(_hass: HomeAssistant, _config_entry: ConfigEntry, _async_add_entities: AddEntitiesCallback):
     if _config_entry.data.get(CONF_MODE) == MODE_OPENTHERM:
-        await sensor.async_setup_entry(_hass, _config_entry, _async_add_entities)
+        await opentherm_sensor.async_setup_entry(_hass, _config_entry, _async_add_entities)
