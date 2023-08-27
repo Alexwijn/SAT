2c7cdfd61a522d8e4bc3c4d7207f5331c22da71b
Alex Wijnholds <alexw@asclub.eu>
2023-08-27 17:10:37 +0200

    Fixed the tests by adding a default heating system

--- tests/test_climate.py
+++ tests/test_climate.py
@@ -17,6 +17,7 @@ from custom_components.sat.fake import SatFakeCoordinator
     [(
             [(TEMPLATE_DOMAIN, 1)],
             {
+                CONF_HEATING_SYSTEM: HEATING_SYSTEM_RADIATORS,
                 CONF_MINIMUM_SETPOINT: 57,
                 CONF_MAXIMUM_SETPOINT: 75,
                 CONF_HEATING_CURVE_COEFFICIENT: 1.8
@@ -56,6 +57,7 @@ async def test_scenario_1(hass: HomeAssistant, entry: MockConfigEntry, climate: 
     [(
             [(TEMPLATE_DOMAIN, 1)],
             {
+                CONF_HEATING_SYSTEM: HEATING_SYSTEM_RADIATORS,
                 CONF_MINIMUM_SETPOINT: 58,
                 CONF_MAXIMUM_SETPOINT: 75,
                 CONF_HEATING_CURVE_COEFFICIENT: 1.3
@@ -96,6 +98,7 @@ async def test_scenario_2(hass: HomeAssistant, entry: MockConfigEntry, climate: 
     [(
             [(TEMPLATE_DOMAIN, 1)],
             {
+                CONF_HEATING_SYSTEM: HEATING_SYSTEM_RADIATORS,
                 CONF_MINIMUM_SETPOINT: 41,
                 CONF_MAXIMUM_SETPOINT: 75,
                 CONF_HEATING_CURVE_COEFFICIENT: 0.9
