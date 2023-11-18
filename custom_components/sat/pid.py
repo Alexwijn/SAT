ba30a3c3bc77d7c4ec4ba24d0f65d3ac5788a56a
Alex Wijnholds <alexw@asclub.eu>
2023-11-18 16:45:56 +0100

    Typo?

--- custom_components/sat/pid.py
+++ custom_components/sat/pid.py
@@ -293,7 +293,7 @@ class PID:
         derivative = self.kd * self._raw_derivative
         output = self._last_heating_curve_value + self.proportional + self.integral
 
-        if abs(derivative) <= 0 or abs(self._last_boiler_temperature - output) < 3:
+        if derivative <= 0 or abs(self._last_boiler_temperature - output) < 3:
             return 0
 
         return round(derivative, 3)
