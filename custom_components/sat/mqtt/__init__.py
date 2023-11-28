7421adc4d637d306ec2130bd0a17bdcfab72fc5e
Alex Wijnholds <alexw@asclub.eu>
2023-11-28 15:44:12 +0100

    Use direct values, again...

--- custom_components/sat/mqtt/__init__.py
+++ custom_components/sat/mqtt/__init__.py
@@ -8,10 +8,9 @@ from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAI
 from homeassistant.components.mqtt import DOMAIN as MQTT_DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import STATE_UNKNOWN, STATE_UNAVAILABLE
-from homeassistant.core import HomeAssistant, Event, State
+from homeassistant.const import STATE_UNAVAILABLE, STATE_UNKNOWN
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry, entity_registry
-from homeassistant.helpers.event import async_track_state_change_event
 
 from ..const import *
 from ..coordinator import DeviceState, SatDataUpdateCoordinator
@@ -33,16 +32,6 @@ if typing.TYPE_CHECKING:
     from ..climate import SatClimate
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
-
-
-def entity_id_to_opentherm_key(hass: HomeAssistant, node_id: str, entity_id: str):
-    entities = entity_registry.async_get(hass)
-    entity = entities.async_get(entity_id)
-
-    if entity.unique_id:
-        return entity.unique_id[len(node_id) + 1:]
-
-    return None
 
 
 class SatMqttCoordinator(SatDataUpdateCoordinator):
@@ -77,106 +66,80 @@ class SatMqttCoordinator(SatDataUpdateCoordinator):
 
     @property
     def device_active(self) -> bool:
-        return self.data.get(DATA_CENTRAL_HEATING) == DeviceState.ON
+        return self._get_entity_state(BINARY_SENSOR_DOMAIN, DATA_CENTRAL_HEATING) == DeviceState.ON
 
     @property
     def flame_active(self) -> bool:
-        return self.data.get(DATA_FLAME_ACTIVE) == DeviceState.ON
+        return self._get_entity_state(BINARY_SENSOR_DOMAIN, DATA_FLAME_ACTIVE) == DeviceState.ON
 
     @property
     def hot_water_active(self) -> bool:
-        return self.data.get(DATA_DHW_ENABLE) == DeviceState.ON
+        return self._get_entity_state(BINARY_SENSOR_DOMAIN, DATA_DHW_ENABLE) == DeviceState.ON
 
     @property
     def setpoint(self) -> float | None:
-        if (setpoint := self.data.get(DATA_CONTROL_SETPOINT)) is not None:
+        if (setpoint := self._get_entity_state(SENSOR_DOMAIN, DATA_CONTROL_SETPOINT)) is not None:
             return float(setpoint)
 
         return None
 
     @property
     def hot_water_setpoint(self) -> float | None:
-        if (setpoint := self.data.get(DATA_DHW_SETPOINT)) is not None:
+        if (setpoint := self._get_entity_state(SENSOR_DOMAIN, DATA_DHW_SETPOINT)) is not None:
             return float(setpoint)
 
         return None
 
     @property
     def minimum_hot_water_setpoint(self) -> float:
-        if (setpoint := self.data.get(DATA_DHW_SETPOINT_MINIMUM)) is not None:
+        if (setpoint := self._get_entity_state(SENSOR_DOMAIN, DATA_DHW_SETPOINT_MINIMUM)) is not None:
             return float(setpoint)
 
         return super().minimum_hot_water_setpoint
 
     @property
     def maximum_hot_water_setpoint(self) -> float | None:
-        if (setpoint := self.data.get(DATA_DHW_SETPOINT_MAXIMUM)) is not None:
+        if (setpoint := self._get_entity_state(SENSOR_DOMAIN, DATA_DHW_SETPOINT_MAXIMUM)) is not None:
             return float(setpoint)
 
         return super().maximum_hot_water_setpoint
 
     @property
     def boiler_temperature(self) -> float | None:
-        if (value := self.data.get(DATA_BOILER_TEMPERATURE)) is not None:
+        if (value := self._get_entity_state(SENSOR_DOMAIN, DATA_BOILER_TEMPERATURE)) is not None:
             return float(value)
 
         return super().boiler_temperature
 
     @property
     def relative_modulation_value(self) -> float | None:
-        if (value := self.data.get(DATA_REL_MOD_LEVEL)) is not None:
+        if (value := self._get_entity_state(SENSOR_DOMAIN, DATA_REL_MOD_LEVEL)) is not None:
             return float(value)
 
         return None
 
     @property
     def boiler_capacity(self) -> float | None:
-        if (value := self.data.get(DATA_BOILER_CAPACITY)) is not None:
+        if (value := self._get_entity_state(SENSOR_DOMAIN, DATA_BOILER_CAPACITY)) is not None:
             return float(value)
 
         return super().boiler_capacity
 
     @property
     def minimum_relative_modulation_value(self) -> float | None:
-        if (value := self.data.get(DATA_REL_MIN_MOD_LEVEL)) is not None:
+        if (value := self._get_entity_state(SENSOR_DOMAIN, DATA_REL_MIN_MOD_LEVEL)) is not None:
             return float(value)
 
         # Legacy
-        if (value := self.data.get(DATA_REL_MIN_MOD_LEVELL)) is not None:
+        if (value := self._get_entity_state(SENSOR_DOMAIN, DATA_REL_MIN_MOD_LEVELL)) is not None:
             return float(value)
 
         return super().boiler_capacity
 
-    async def async_added_to_hass(self, climate: SatClimate = None) -> None:
+    async def async_added_to_hass(self, climate: SatClimate) -> None:
         await mqtt.async_wait_for_mqtt_client(self.hass)
 
         await self._send_command("PM=48")
-
-        entities = list(filter(lambda entity: entity is not None, [
-            self._get_entity_id(BINARY_SENSOR_DOMAIN, DATA_CENTRAL_HEATING),
-            self._get_entity_id(BINARY_SENSOR_DOMAIN, DATA_FLAME_ACTIVE),
-            self._get_entity_id(BINARY_SENSOR_DOMAIN, DATA_DHW_ENABLE),
-
-            self._get_entity_id(SENSOR_DOMAIN, DATA_DHW_SETPOINT),
-            self._get_entity_id(SENSOR_DOMAIN, DATA_CONTROL_SETPOINT),
-            self._get_entity_id(SENSOR_DOMAIN, DATA_REL_MOD_LEVEL),
-            self._get_entity_id(SENSOR_DOMAIN, DATA_BOILER_TEMPERATURE),
-            self._get_entity_id(SENSOR_DOMAIN, DATA_BOILER_CAPACITY),
-            self._get_entity_id(SENSOR_DOMAIN, DATA_REL_MIN_MOD_LEVEL),
-            self._get_entity_id(SENSOR_DOMAIN, DATA_REL_MIN_MOD_LEVELL),
-            self._get_entity_id(SENSOR_DOMAIN, DATA_DHW_SETPOINT_MINIMUM),
-            self._get_entity_id(SENSOR_DOMAIN, DATA_DHW_SETPOINT_MAXIMUM),
-        ]))
-
-        for entity_id in entities:
-            if state := self.hass.states.get(entity_id):
-                await self._on_state_change(entity_id, state)
-
-        async def async_coroutine(event: Event):
-            await self._on_state_change(event.data.get("entity_id"), event.data.get("new_state"))
-
-        async_track_state_change_event(self.hass, entities, async_coroutine)
-
         await super().async_added_to_hass(climate)
 
     async def async_set_control_setpoint(self, value: float) -> None:
@@ -209,23 +172,15 @@ class SatMqttCoordinator(SatDataUpdateCoordinator):
 
         await super().async_set_control_max_setpoint(value)
 
+    def _get_entity_state(self, domain: str, key: str):
+        state = self.hass.states.get(self._get_entity_id(domain, key))
+        if state.state is None or state.state in (STATE_UNAVAILABLE, STATE_UNKNOWN):
+            return None
+
+        return state.state
+
     def _get_entity_id(self, domain: str, key: str):
         return self._entity_registry.async_get_entity_id(domain, MQTT_DOMAIN, f"{self._node_id}-{key}")
-
-    async def _on_state_change(self, entity_id: str, state: State):
-        key = entity_id_to_opentherm_key(self.hass, self._node_id, entity_id)
-        if key is None:
-            return
-
-        if state.state is None or state.state in (STATE_UNAVAILABLE, STATE_UNKNOWN):
-            self.data[key] = None
-        else:
-            self.data[key] = state.state
-
-        if self._listeners:
-            self._schedule_refresh()
-
-        self.async_update_listeners()
 
     async def _send_command(self, payload: str):
         if not self._simulation:
