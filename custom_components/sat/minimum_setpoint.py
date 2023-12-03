0c57c83a77f5d79c8d31f5478d038ab00c06c4fd
Alex Wijnholds <alexw@asclub.eu>
2023-12-03 19:32:55 +0100

    Do nothing when the temperature is below target

--- custom_components/sat/minimum_setpoint.py
+++ custom_components/sat/minimum_setpoint.py
@@ -7,23 +7,18 @@ class MinimumSetpoint:
         self._coordinator = coordinator
         self._previous_minimum_setpoint = None
 
-    def calculate(self, adjustment_percentage=10) -> float:
+    def calculate(self, adjustment_percentage=10) -> None:
         # Extract relevant values from the coordinator for clarity
         boiler_temperature = self._coordinator.boiler_temperature
         target_setpoint_temperature = self._coordinator.setpoint
         minimum_setpoint = self._coordinator.minimum_setpoint
         is_flame_active = self._coordinator.flame_active
 
-        # Check if either boiler_temperature or target_setpoint_temperature is None
         if boiler_temperature is None or target_setpoint_temperature is None:
-            return minimum_setpoint
+            return
 
-        # Check if the boiler temperature is stable at the target temperature
-        is_temperature_stable = abs(boiler_temperature - target_setpoint_temperature) <= 1
-
-        if is_temperature_stable:
-            # Boiler temperature is stable, return the coordinator's minimum setpoint
-            return minimum_setpoint
+        if boiler_temperature < (target_setpoint_temperature - 1):
+            return
 
         # Calculate the adjustment value based on the specified percentage
         adjustment_value = (adjustment_percentage / 100) * (target_setpoint_temperature - boiler_temperature)
