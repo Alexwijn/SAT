621e72a1a297b6a2104362180eccfbd0e6a496eb
Alex Wijnholds <alexw@asclub.eu>
2024-11-23 15:57:29 +0100

    Lower the Ki denominator

--- custom_components/sat/pid.py
+++ custom_components/sat/pid.py
@@ -299,7 +299,7 @@ class PID:
                 return round(self._last_heating_curve_value / 73900, 6)
 
             if self._version == 2:
-                return round(self._automatic_gains_value * (self._last_heating_curve_value / 36000), 6)
+                return round(self._automatic_gains_value * (self._last_heating_curve_value / 7200), 6)
 
             raise Exception("Invalid version")
 
